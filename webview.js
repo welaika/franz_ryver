672184516c46d9fd1fc365dbf425abe0f9393ff2
Filippo Gangi Dino <filippo.gangidino@welaika.com>
2017-06-21 16:57:48 +0200

    fix typo

--- webview.js
+++ webview.js
@@ -5,7 +5,7 @@ module.exports = (Franz, options) => {
     let team_notifications = document.querySelectorAll('.scene-space__drawer  [data-entity="Entity.Workroom"].scene-space-tab-button--flash>.scene-space-tab-button__flash');
     let notification_counter = parseInt(notifications.innerText);
     let personal_notification_counter = direct_notifications.length;
-    let teams_notification_counter = direct_notifications.length;
+    let teams_notification_counter = team_notifications.length;
     let direct_notification_counter = personal_notification_counter + teams_notification_counter;
     Franz.setBadge(direct_notification_counter, notification_counter);
 	}
